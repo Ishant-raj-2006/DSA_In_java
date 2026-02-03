c62df85c2c7a9b12271efb90d32e8caf0a1bc908
ishant-raj-2006 <ishant786raj@gmail.com>
2026-02-03 20:09:28 +0530

    Aaray today 21%

---
+++ Day6.java/Q7.java
@@ -0,0 +1,11 @@
+// WAP for print 3 student names by using Array.
+
+public class Q7 {
+
+    public static void main(String[] args) {
+        String[] arr={"Ishant","Raj","janu"};
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
