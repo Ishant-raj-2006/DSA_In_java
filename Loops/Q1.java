6fa9744fa6cd8cce9c70e718b7de4c45f8407ad2
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-07-20 21:40:39 +0530

    Loops Q1

---
+++ Loops/Q1.java
@@ -0,0 +1,9 @@
+// WAP for print this series - 99,95,91,87........upto all terms which are positive.
+public class Q1 {
+    public static void main(String[] args) {
+       for(int i=99; i>=0; i= i-4){
+        System.out.print(i +" ");
+       }
+    }
+    
+}
