f69edf263c17d9dc0ed8dfdd61bf4abdc066e743
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-07-22 10:32:52 +0530

    Q4

---
+++ Q4.java
@@ -0,0 +1,8 @@
+// WAP for print all alphabets with their corresponding ASCII Values.
+public class Q4{
+    public static void main(String[] args) {
+      for (int i=65; i<=90; i++){
+        System.out.println((char)i + "-->" +i);
+      }
+    }
+}
