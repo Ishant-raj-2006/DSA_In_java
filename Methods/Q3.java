5923c92db5bccbcfd891841b305f57a1c6b0475d
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-08-08 17:37:48 +0530

    Q4

--- Methods/Q3.java
+++ Methods/Q3.java
@@ -1,11 +1,11 @@
-// WAP for find Squre root of any number 
+// WAP for find Absoulnt of any number 
 
 import java.util.Scanner;
-public class Q2 {
+public class Q3 {
     public static void main(String[] args) {
         Scanner sb = new Scanner(System.in);
         System.out.print("Enter Number = ");
         int num = sb.nextInt();
-        System.out.println(Math.sqrt(num));
+        System.out.println(Math.abs(num));
     }
 }
--- Methods/Q4.java
+++ Methods/Q4.java
@@ -1,11 +1,11 @@
-// WAP for find Squre root of any number 
+// WAP for find Floor  of any number 
 
 import java.util.Scanner;
-public class Q2 {
+public class Q4 {
     public static void main(String[] args) {
         Scanner sb = new Scanner(System.in);
         System.out.print("Enter Number = ");
-        int num = sb.nextInt();
-        System.out.println(Math.sqrt(num));
+        double num = sb.nextDouble();
+        System.out.print(Math.floor(num));
     }
 }
