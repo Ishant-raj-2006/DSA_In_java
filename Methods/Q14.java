637d742880361f9ad9caa59cea45104176b2c9c9
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-08-11 07:54:18 +0530

    Q14

---
+++ Methods/Q14.java
@@ -0,0 +1,10 @@
+// WAP for  Print Your name by using. 
+
+public class Q14 {
+public static void Ishant(){
+    System.out.println("Hello Ishant");
+}
+    public static void main(String[] args) {
+        Ishant();
+    }
+}
