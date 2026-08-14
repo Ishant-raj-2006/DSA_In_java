cc558cb0e8364138cd42ccbb34c76596dfb24f14
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-08-14 10:05:36 +0530

    Q1

---
+++ Arrays/Q1.java
@@ -0,0 +1,15 @@
+// WAP for print the all element of arrays
+
+public  class Q1 {
+
+public static void main(String[] args) {
+    int[] arr ={5,6,2,76,435,213,1324,46546,325};
+    int i=0;
+    while(i<=arr.length){
+        System.out.print(arr[i] + " ");
+        i++;
+
+    }
+}
+    
+}
